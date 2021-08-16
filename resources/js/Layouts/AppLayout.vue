b1d9d08545521c8285c934c224edae3c8df9308d
Raul A Perusquia <raul@inikoo.com>
2021-08-17 04:57:14 +0800

    AK-6 Script to automatically retrieve translation from sources

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -70,7 +70,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                           d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
                                 </svg>
-                                {{ $trans('Dashboard') }}
+                                {{ __('Dashboard') }}
                             </a>
 
                             <a href="#" class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md">
@@ -130,7 +130,7 @@
                                      {{ $page.props.user.name }}
                                 </p>
                                 <p class="text-sm font-medium text-gray-500 group-hover:text-gray-700">
-                                    View profile
+                                     {{ __('View profile') }}
                                 </p>
                             </div>
                         </div>
@@ -161,7 +161,7 @@
                                       Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500"
                                     -->
                                     <font-awesome-icon class="text-gray-500  mr-3 h-6 w-6" :icon="['far', 'tachometer-alt-fast']" />
-                                    {{ $trans('Dashboard') }}
+                                    {{ __('Dashboard') }}
                                 </a>
                                 <a href="#" class="bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
                                     <!--
@@ -182,7 +182,7 @@
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                                     </svg>
 
-                                    xxxx
+                                    test
                                 </a>
 
                                 <a href="#" class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
@@ -235,7 +235,7 @@
                                     </p>
 
                                     <p class="text-xs font-medium text-gray-500 group-hover:text-gray-700">
-                                        <Link :href="route('profile.show')">View profile</Link>
+                                        <Link :href="route('profile.show')">{{ __('View profile') }} </Link>
 
                                     </p>
                                 </div>
@@ -288,7 +288,7 @@
 <script>
 import {Head, Link} from '@inertiajs/inertia-vue3';
 import JetApplicationMark from "@/Jetstream/ApplicationMark";
-import {__, trans, setLocale, getLocale, transChoice, MaticeLocalizationConfig, locales} from "matice"
+import {__} from "matice"
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -305,7 +305,7 @@ export default {
         FontAwesomeIcon
     },
     methods: {
-        $trans: trans,
+        __: __,
     }
 
 }
