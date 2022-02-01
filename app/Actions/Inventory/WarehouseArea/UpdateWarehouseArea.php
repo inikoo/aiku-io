9a77834540c9d3036712395800ad2dab4ab3f1ac
Raul A Perusquia <raul@inikoo.com>
2022-02-01 14:30:11 +0800

    AK-161 UI Warehouse Area Show/ Edit

--- app/Actions/Inventory/WarehouseArea/UpdateWarehouseArea.php
+++ app/Actions/Inventory/WarehouseArea/UpdateWarehouseArea.php
@@ -8,16 +8,25 @@
 
 namespace App\Actions\Inventory\WarehouseArea;
 
+use App\Http\Resources\Utils\ActionResultResource;
 use App\Models\Utils\ActionResult;
 use App\Actions\WithUpdate;
 use App\Models\Inventory\WarehouseArea;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Lorisleiva\Actions\ActionRequest;
 use Lorisleiva\Actions\Concerns\AsAction;
+use Lorisleiva\Actions\Concerns\WithAttributes;
 
+/**
+ * @property WarehouseArea $warehouseArea
+ */
 class UpdateWarehouseArea
 {
     use AsAction;
     use WithUpdate;
+    use WithAttributes;
 
     public function handle(WarehouseArea $area, array $modelData): ActionResult
     {
@@ -33,4 +42,56 @@ class UpdateWarehouseArea
 
         return $res;
     }
+
+    public function authorize(ActionRequest $request): bool
+    {
+        return $request->user()->tokenCan('root')
+            || $request->user()->tokenCan('inventory:edit')
+            || $request->user()->hasPermissionTo("warehouses.edit.{$this->warehouseArea->warehouse_id}");
+    }
+
+    public function rules(): array
+    {
+        return [
+            'code' => 'sometimes|required|alpha_dash|unique:App\Models\Inventory\WarehouseArea,code',
+            'name' => 'sometimes|required|unique:App\Models\Inventory\WarehouseArea,name',
+
+        ];
+    }
+
+    public function asController(WarehouseArea $warehouseArea, ActionRequest $request): ActionResultResource
+    {
+        $request->validate();
+
+
+        return new ActionResultResource(
+            $this->handle(
+                $warehouseArea,
+                $request->only(
+                    'code',
+                )
+            )
+        );
+    }
+
+    public function asInertia(WarehouseArea $warehouseArea, Request $request): RedirectResponse
+    {
+        $this->set('warehouseArea', $warehouseArea);
+        $this->fillFromRequest($request);
+        $this->validateAttributes();
+
+
+        $this->handle(
+            $warehouseArea,
+            $request->only(
+                'code',
+            )
+        );
+
+        return redirect()->back();
+
+
+    }
+
 }
+
